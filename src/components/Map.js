71a2668eafc975e1600887093c9ddd5ecdf57135
Maria <ag.mar.py@gmail.com>
2018-08-02 18:24:12 +0300

    composed map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,40 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import {compose, withProps} from "recompose"
+import { withGoogleMap, GoogleMap, Marker, InfoWindow, withScriptjs } from 'react-google-maps';
 
-class Map extends Component {  //Help for the map taken from https://bit.ly/2MbUOGi
-  static propTypes = {
 
-  }
-   render() {
-   const CompositeGoogleMap = withGoogleMap(props => (
+  const MyGoogleMapComponent = compose(
+     withProps({
+       googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC8v53M-4E8CxXiqbvKVMxkSgdpfn2aiaw",
+       loadingElement: <div style={{ height: '100%' }} />,
+       containerElement: <div style={{height: '500px', width: '100%'}} />,
+       mapElement: <div style={{ height: '100%' }} />,
+     }),
+   withScriptjs,
+   withGoogleMap
+ )((props) =>
       <GoogleMap
-        defaultCenter = { { lat: 37.9838096, lng: 23.7275388 } }
         defaultZoom = { 13 }
+        defaultCenter = { { lat: 37.9838096, lng: 23.7275388 } }
       >
+      {props.isMarkerShown && <Marker position = {{lat:37.9838096, lng: 23.7275388 }}/>}
       </GoogleMap>
-   ));
-   return(
+   );
+class Map extends Component {  //Help for the map taken from https://bit.ly/2MbUOGi
+   render() {
+     return(
       <div>
-        <CompositeGoogleMap //markers = {markers}
+        <MyGoogleMapComponent //markers = {markers}
         /*onMarkerClicked = {onLocationClicked}
         appCenter = {newCenter}
         onToggleOpen = {handleToggleOpen}
         showInfoIndex = {showInfoIndex}
         markerIcon = {markerIcon}
         zoom = {zoom}*/
-        containerElement={ <div style={{ height: `500px`, width: '75%' }} /> }
-        mapElement={ <div style={{ height: `100%` }} /> }
         />
       </div>
-   );
-   }
-};
-export default Map;
+   )
+  }
+ };
+
+ export default MyGoogleMapComponent
